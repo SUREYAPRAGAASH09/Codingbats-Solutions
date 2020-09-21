3c87e2d2ca4f667d1fd69460097606020438f4d7
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-21 08:01:19 +0530

    initial commit

---
+++ Warmup-1/parrotTrouble.cs
@@ -0,0 +1,20 @@
+PROBLEM STATEMENT :
+--------------------
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
+We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+SAMPLE TEST CASE :
+------------------
+parrotTrouble(true, 6) → true
+parrotTrouble(true, 7) → false
+parrotTrouble(false, 6) → false
+
+MY SOLUTION :
+-------------
+public boolean parrotTrouble(boolean talking, int hour) {
+  if ( (talking) && ((7>hour)||(hour>20)) )
+  {
+     return true;
+  }
+  return false;
+}

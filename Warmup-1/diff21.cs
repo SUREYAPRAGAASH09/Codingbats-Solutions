3b9e5ff70fa96326c4aacf711391ad32cd9a9e4a
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-21 07:59:31 +0530

    intial commit

---
+++ Warmup-1/diff21.cs
@@ -0,0 +1,22 @@
+PROBLEM STATEMENT :
+-------------------
+Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+SAMPLE TEST CASES :
+-------------------
+diff21(19) → 2
+diff21(10) → 11
+diff21(21) → 0
+
+MY SOLUTION :
+-------------
+public int diff21(int n) {
+  if (n > 21)
+  {
+    return Math.abs(n - 21) * 2;
+  }
+  else
+  {
+    return Math.abs(n - 21);
+  }
+}

2644b59766a3e04420ffb9f2fc5f65db7213ecde
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-23 21:45:24 +0530

    intial commit

---
+++ Warmup-1/backAround.java
@@ -0,0 +1,21 @@
+Problem Statement :
+-------------------
+          Given a string, take the last char and return a new string with the last char added at the front and back, 
+          so "cat" yields "tcatt". The original string will be length 1 or more.
+          
+Test Cases :
+------------
+          backAround("cat") → "tcatt"
+          backAround("Hello") → "oHelloo"
+          backAround("a") → "aaa"
+
+My Solution :
+-------------
+          public String backAround(String str) {
+            if (str.length() >= 2) {
+                  return str.substring(str.length() - 1, str.length()) + str + str.substring(str.length() - 1, str.length());
+            }	    
+            else {
+                  return str + str + str;
+            }
+          }

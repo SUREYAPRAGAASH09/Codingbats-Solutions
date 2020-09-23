a252191be87a5b4df5ee87b34b646c4c931dd3d7
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-23 09:43:41 +0530

    second Solution added

--- Warmup-1/notString.java
+++ Warmup-1/notString.java
@@ -10,9 +10,26 @@ Test Cases :
     notString("x") → "not x"
     notString("not bad") → "not bad"
 
-My Solution :
+My Solutions :
 -------------
+    Solution 1 :
+    ------------
     public String notString(String str) {
           if (str.trim().split("\\s+")[0].equals("not"))
               return str;      
           return "not " + str;
+          
+    solution 2 :
+    ------------
+    public String notString(String str) {
+          try {
+                if ((str.charAt(0) == 'n') && (str.charAt(1) == 'o') && (str.charAt(2) == 't') ) 
+                        return str;
+                else
+                      return "not00" + str;
+          }
+          catch(Exception e) {
+		    return "not " + str;
+	     }
+    }
+      

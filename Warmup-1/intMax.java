6f03235f018a7d9c7217b748d1643df70e1a7560
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-24 12:38:32 +0530

    intial commit

---
+++ Warmup-1/intMax.java
@@ -0,0 +1,24 @@
+Problem Statement :
+-------------------
+    Given three int values, a b c, return the largest.
+
+Test Cases :
+------------
+    intMax(1, 2, 3) → 3
+    intMax(1, 3, 2) → 3
+    intMax(3, 2, 1) → 3
+    
+My Solution :
+-------------
+    public int intMax(int a, int b, int c) {
+      int max;
+      if ((a>b) && (a>c))
+          max = a;
+      else if ((b>c)  && (b>a))
+          max = b;
+      else
+          max = c;
+      return max;
+     }
+    
+   

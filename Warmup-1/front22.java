363bbfdcae0a6951ed0ea173981b20f621df4a82
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-24 10:16:57 +0530

    intial commit

---
+++ Warmup-1/front22.java
@@ -0,0 +1,21 @@
+Problem Statement :
+--------------------
+    Given a string, take the first 2 chars and return the string with the 2 chars added at both the 
+  front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+
+Test Cases:
+-----------
+    front22("kitten") → "kikittenki"
+    front22("Ha") → "HaHaHa"
+    front22("abc") → "ababcab"
+    
+My solution :
+------------
+public String front22(String str) {
+  if (str.length() >= 2) {
+    String FirstTwoChar = Character.toString(str.charAt(0)) + Character.toString(str.charAt(1));
+    return  FirstTwoChar + str + FirstTwoChar;
+  }
+  else 
+		    return str + str + str;
+}

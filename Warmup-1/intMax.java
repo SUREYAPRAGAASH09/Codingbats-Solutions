c79c777ff2997ec8f9de3fec07161bbd36947b7b
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-24 12:40:37 +0530

    second Solution added

--- Warmup-1/intMax.java
+++ Warmup-1/intMax.java
@@ -20,5 +20,8 @@ My Solution :
           max = c;
       return max;
      }
-    
-   
+My Solution 2 : [Code optimized too much]
+---------------
+    public int intMax(int a, int b, int c) {
+       return ((a>b) && (a>c)) ? a : ((b>c)  && (b>a)) ? b : c; 
+    }

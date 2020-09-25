b3730b5fc14c338d251cc6289142a297a9c3f180
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-25 22:11:08 +0530

    intial commit

---
+++ Warmup-1/in3050.java
@@ -0,0 +1,16 @@
+Problem Statement :
+-------------------
+      Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
+
+Test cases :
+------------
+      in3050(30, 31) → true
+      in3050(30, 41) → false
+      in3050(40, 50) → true
+      
+My Solution :
+-------------
+      public boolean in3050(int a, int b) 
+      {
+        return ( ((a>=30&&a<=40) && (b>=30&&b<=40)) || ((a>=40&&a<=50) && (b>=40&&b<=50))) ? true : false; 
+      }

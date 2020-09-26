6248774ef170b14912e83266682ee73d486ffdef
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-26 21:41:35 +0530

    intial commit

---
+++ Warmup-1/loneTeen.java
@@ -0,0 +1,23 @@
+Problem Statement :
+-------------------
+      We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+
+
+Test cases :
+------------
+      loneTeen(13, 99) → true
+      loneTeen(21, 19) → true
+      loneTeen(13, 13) → false
+
+My Solution :
+-------------
+      public boolean loneTeen(int a, int b) {
+        boolean ind = true;
+            if ( ( a >= 13 && a <= 19 ) && ( b >= 13 && b <= 19 ) )
+                ind = false;
+            else if ( ( a >= 13 && a <= 19 ) || ( b >= 13 && b <= 19 ) )
+              ind = true;
+            else
+              ind = false;
+        return ind;
+      }

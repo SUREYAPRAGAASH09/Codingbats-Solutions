78f20a3392b1d3a97ac6bcbe929cb2a23e14899b
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-28 19:47:55 +0530

    Second Solution added
    existing solution optimized using conditional operator insist of using regular if else statement.

--- Warmup-1/diff21.java
+++ Warmup-1/diff21.java
@@ -1,22 +1,28 @@
 PROBLEM STATEMENT :
 -------------------
-Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+          Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
 
 SAMPLE TEST CASES :
 -------------------
-diff21(19) → 2
-diff21(10) → 11
-diff21(21) → 0
+        diff21(19) → 2
+        diff21(10) → 11
+        diff21(21) → 0
 
 MY SOLUTION :
 -------------
-public int diff21(int n) {
-  if (n > 21)
-  {
-    return Math.abs(n - 21) * 2;
-  }
-  else
-  {
-    return Math.abs(n - 21);
-  }
-}
+        public int diff21(int n) {
+          if (n > 21)
+          {
+            return Math.abs(n - 21) * 2;
+          }
+          else
+          {
+            return Math.abs(n - 21);
+          }
+        }
+
+My Solution 2 :
+--------------
+        public int diff21(int n) {
+          return (n > 21) ? Math.abs(n - 21) * 2 :  Math.abs(n - 21);
+        }

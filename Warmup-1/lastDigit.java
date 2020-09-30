924caaf953314254db1c661568d91daa305fc8d4
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-30 22:01:07 +0530

    intial commit

--- Warmup-1/lastDigit.java
+++ Warmup-1/lastDigit.java
@@ -1,7 +1,7 @@
 Problem Statement :
 -------------------
       Given two non-negative int values, return true if they have the same last digit, 
-      such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+      such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.  
 
 Test cases :
 ------------

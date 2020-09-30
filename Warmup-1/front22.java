7b37b5188af29f5526da725029dc43192000ee17
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-30 22:00:19 +0530

    intial commit

--- Warmup-1/front22.java
+++ Warmup-1/front22.java
@@ -1,7 +1,7 @@
 Problem Statement :
 --------------------
     Given a string, take the first 2 chars and return the string with the 2 chars added at both the 
-  front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+    front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
 
 Test Cases:
 -----------
@@ -11,11 +11,10 @@ Test Cases:
     
 My solution :
 ------------
-public String front22(String str) {
-  if (str.length() >= 2) {
-    String FirstTwoChar = Character.toString(str.charAt(0)) + Character.toString(str.charAt(1));
-    return  FirstTwoChar + str + FirstTwoChar;
-  }
-  else 
-		    return str + str + str;
-}
+   public String front22(String str) {
+      if (str.length() >= 2) {
+      String FirstTwoChar = Character.toString(str.charAt(0)) + Character.toString(str.charAt(1));
+      return  FirstTwoChar + str + FirstTwoChar;
+      } else 
+	  return str + str + str;
+   }

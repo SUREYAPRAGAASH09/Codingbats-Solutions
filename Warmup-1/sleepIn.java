d4569e8beaa640b5ab818f05edb9df0cfe57fb5a
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-12-16 06:38:01 +0530

    Solution updated

--- Warmup-1/sleepIn.java
+++ Warmup-1/sleepIn.java
@@ -1,22 +1,22 @@
 PROEBLEM STATEMENT : 
 ---------------------
-  The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
-  We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
+    We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
 
 Sample Test case :
 -----------------
-sleepIn(false, false) → true
-sleepIn(true, false) → false
-sleepIn(false, true) → true
+    sleepIn(false, false) → true
+    sleepIn(true, false) → false
+    sleepIn(false, true) → true
 
 My Solution :
 -------------
-public boolean sleepIn(boolean weekday, boolean vacation) 
-{
-    boolean SleepIn = false;
-    if ( !(weekday) || (vacation) )
+    public boolean sleepIn(boolean weekday, boolean vacation) 
     {
-        SleepIn = true;
+        boolean SleepIn = false;
+  
+        if ( !(weekday) || (vacation) )
+            SleepIn = true;
+        
+        return SleepIn;
     }
-    return SleepIn;
-}

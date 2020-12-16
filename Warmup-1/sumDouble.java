8c8223406bd1c881fb55392b9416d1fdaa4bfe5a
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-12-16 06:34:53 +0530

    new Solution added

--- Warmup-1/sumDouble.java
+++ Warmup-1/sumDouble.java
@@ -1,22 +1,40 @@
 PROBLEM STATEMENT :
 --------------------
-Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+    Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 
 SAMPLE TEST CASES :
 -------------------
-sumDouble(1, 2) → 3
-sumDouble(3, 2) → 5
-sumDouble(2, 2) → 8
+    sumDouble(1, 2) → 3
+    sumDouble(3, 2) → 5
+    sumDouble(2, 2) → 8
 
 MY SOLUTION :
 -------------
-public int sumDouble(int a, int b) {
-  if (a == b)
-  {
-    return ((a + b) + (a + b));
-  }
-  else
-  {
-    return a + b;
-  }
+    public int sumDouble(int a, int b) {
+      if (a == b)
+      {
+        return ((a + b) + (a + b));
+      }
+      else
+      {
+        return a + b;
+      }
+    }
+
+MY SOLUTION 2 :
+-------------
+    public int sumDouble(int a, int b) {
+      return (a == b) ? 2* (a+b) : (a+b);
+    }
+
+
+My Solution 3 :
+---------------
+  public int sumDouble(int a, int b) {
+      boolean AbsotuleSubtractionFlag = false;
+
+      if ((a-b) == 0)
+        AbsotuleSubtractionFlag = true;
+
+      return (AbsotuleSubtractionFlag) ? 2* (a+b) : (a+b);
 }

fec6fb08f8fbeba10c3f6ff2e87f5c9b3d2d0fdd
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-12-16 06:12:20 +0530

    Solution modified

--- Warmup-1/parrotTrouble.java
+++ Warmup-1/parrotTrouble.java
@@ -1,20 +1,20 @@
 PROBLEM STATEMENT :
 --------------------
-We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
-We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+      We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
+      We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
 
 SAMPLE TEST CASE :
 ------------------
-parrotTrouble(true, 6) → true
-parrotTrouble(true, 7) → false
-parrotTrouble(false, 6) → false
+    parrotTrouble(true, 6) → true
+    parrotTrouble(true, 7) → false
+    parrotTrouble(false, 6) → false
 
 MY SOLUTION :
 -------------
-public boolean parrotTrouble(boolean talking, int hour) {
-  if ( (talking) && ((7>hour)||(hour>20)) )
-  {
-     return true;
-  }
-  return false;
-}
+    public boolean parrotTrouble(boolean talking, int hour) {
+      if ( (talking) && ((7>hour)||(hour>20)) )
+      {
+         return true;
+      }
+      return false;
+    }

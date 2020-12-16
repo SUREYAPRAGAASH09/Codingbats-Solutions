82de86a3c99ae525998846e32d4abdb492cb1516
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-12-16 06:36:20 +0530

    Solution modified

--- Warmup-1/monkeyTrouble.java
+++ Warmup-1/monkeyTrouble.java
@@ -1,23 +1,17 @@
 PROBLEM STATEMENT :
 -------------------
-    We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
-We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+        We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+    We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
 
 SAMPLE TEST CASE :
 ------------------
-monkeyTrouble(true, true) → true
-monkeyTrouble(false, false) → true
-monkeyTrouble(true, false) → false
+    monkeyTrouble(true, true) → true
+    monkeyTrouble(false, false) → true
+    monkeyTrouble(true, false) → false
 
 MY SOLUTION :
 -------------
-public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-  if ((aSmile && bSmile) || (!(aSmile) && !(bSmile)))
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+      if ((aSmile && bSmile) || (!(aSmile) && !(bSmile)))
+        return true;
+      }

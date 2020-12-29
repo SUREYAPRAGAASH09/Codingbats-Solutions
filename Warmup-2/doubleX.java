ad93f4904cdb74b96ddbffa155343bf5acdb9c43
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-12-29 20:50:47 +0530

    initial commit

---
+++ Warmup-2/doubleX.java
@@ -0,0 +1,37 @@
+Problem Statement :
+-------------------
+    Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+    
+Sample Test cases :
+-------------------
+    doubleX("axxbb") → true
+    doubleX("axaxax") → false
+    doubleX("xxxxx") → true
+    
+My Solution :
+-------------
+    boolean doubleX(String str) 
+    {
+      boolean ResultFlag = false;
+      int countX = 0, temp1 = 0, temp2 = 0;
+      for (int itr = str.length()-1; itr >= 0 ; itr --)
+      {
+          if (str.charAt(itr) == 'x')
+          {
+              countX += 1;
+              if (countX >= 2)
+              {
+                  temp2 = itr;
+                  ResultFlag = (((temp1 - 1) - temp2) == 0 ) ? true : false;
+                  temp1 = temp2;
+                  countX -= 1;  
+              }
+              else if (countX == 1)
+              {
+                  temp1 = itr;
+                  countX += 1;
+              }
+          }
+      }
+      return ResultFlag;
+  }

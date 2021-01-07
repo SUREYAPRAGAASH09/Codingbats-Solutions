c70432a9ee87378c4f256e6fba3897936cca3c6b
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2021-01-07 18:57:07 +0530

    initial commit

---
+++ Warmup-2/stringSplosion.java
@@ -0,0 +1,29 @@
+Problem Statement :
+-------------------
+    Given a non-empty string like "Code" return a string like "CCoCodCode".
+    
+Sample Test cases :
+-------------------
+   stringSplosion("Code") → "CCoCodCode"
+   stringSplosion("abc") → "aababc"
+   stringSplosion("ab") → "aab"
+    
+My Solution :
+-------------
+  public String stringSplosion(String str) 
+  {
+    int strLenCnt = 0, innerLoopCnt = 0, strLen = str.length()-1;
+    String newStr = "";
+
+    while (strLen >= strLenCnt)
+    {
+      innerLoopCnt = 0;
+      while (strLenCnt >= innerLoopCnt)
+      {
+        newStr = newStr + Character.toString(str.charAt(innerLoopCnt));
+        innerLoopCnt += 1;
+      }
+      strLenCnt += 1;
+    }
+    return newStr;
+  }

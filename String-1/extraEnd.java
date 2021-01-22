0abc91e3784692607006c9e292096c3e239507ba
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2021-01-22 18:50:17 +0530

    First Solution Added

---
+++ String-1/extraEnd.java
@@ -0,0 +1,21 @@
+Problem Statement :
+-------------------
+    Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+          
+Test Cases :
+------------
+    extraEnd("Hello") → "lololo"
+    extraEnd("ab") → "ababab"
+    extraEnd("Hi") → "HiHiHi"
+My Solution :
+------------- 
+    public String extraEnd(String str) {
+      int StrLen = str.length();
+      String tempStr = "";
+      if (StrLen <= 2)
+        tempStr = str ;
+      else if (StrLen > 2)
+        tempStr = str.substring(StrLen-2, StrLen);
+
+      return tempStr + tempStr + tempStr; 
+    }

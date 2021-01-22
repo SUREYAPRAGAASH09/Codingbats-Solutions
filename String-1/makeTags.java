0372fffdbc089fd13aabb23786e4f2233b526a0f
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2021-01-22 18:46:33 +0530

    initial commit

---
+++ String-1/makeTags.java
@@ -0,0 +1,16 @@
+Problem Statement :
+-------------------
+    The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. 
+    In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
+    Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+          
+Test Cases :
+------------
+    makeTags("i", "Yay") → "<i>Yay</i>"
+    makeTags("i", "Hello") → "<i>Hello</i>"
+    makeTags("cite", "Yay") → "<cite>Yay</cite>"
+My Solution :
+-------------
+    public String makeTags(String tag, String word) {
+      return "<" + tag + ">" + word + "</" + tag + ">";
+    }

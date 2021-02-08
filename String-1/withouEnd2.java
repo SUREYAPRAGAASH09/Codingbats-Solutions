81136b7a03def95f875c478c74c54983c22cf697
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2021-02-08 11:18:20 +0530

    initial commit

---
+++ String-1/withouEnd2.java
@@ -0,0 +1,15 @@
+Problem Statement :
+-------------------
+    Given a string, return a version without both the first and last char of the string. The string may be any length, including 0.
+          
+Test Cases :
+------------
+    withouEnd2("Hello") → "ell"
+    withouEnd2("abc") → "b"
+    withouEnd2("ab") → ""
+    
+My Solution :
+------------- 
+    public String withouEnd2(String str) {
+    return (str.length() >= 3) ? str.substring(1, str.length()-1)  : "" ;
+  }

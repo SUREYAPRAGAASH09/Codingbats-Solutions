538233e9ff08b955873593b1fcf47ec77054f5f2
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2021-02-08 11:06:19 +0530

    Second Solution added

--- String-1/right2.java
+++ String-1/right2.java
@@ -13,3 +13,23 @@ My Solution :
     int StrLen = str.length();
     return str.substring(StrLen-2, StrLen) + str.substring(0,StrLen-2);
   }
+My Solution 2 :
+---------------
+    public String right2(String str) {
+      int StrLen = str.length(), iterator = 0, exitIterator = iterator = StrLen - 2;
+      String newStr = "";
+      boolean exitCond = true;
+
+      while(exitCond)
+      {
+        newStr = newStr + str.charAt(iterator);
+        iterator+=1;
+
+        if (iterator == StrLen)
+          iterator = 0;
+
+        if (iterator == exitIterator)
+          exitCond = false;
+      }
+      return newStr; 
+    }

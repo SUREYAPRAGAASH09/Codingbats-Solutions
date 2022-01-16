2626f166830ac6b653c19246a351e8e961832996
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2022-01-16 22:29:10 +0530

    second solution updated

--- Warmup-2/countXX.java
+++ Warmup-2/countXX.java
@@ -22,3 +22,19 @@ My Solution :
     }  
     return (count > 0) ? count-1 : 0;
  }
+
+My second solution : 
+---------------------
+    int countXX(String str) 
+    {
+          int Cnt = 0, temp;
+          for (int i=0; i<str.length()-1; i++)
+          {
+            temp = i + 1;
+            if ((str.charAt(i) == 'x') && (str.charAt(temp) == 'x'))
+            {
+              Cnt += 1;
+            }
+          }
+          return Cnt;
+    }

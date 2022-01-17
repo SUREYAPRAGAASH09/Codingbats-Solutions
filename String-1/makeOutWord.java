7a12aa2bdad9cad3b038ed252e127a78f90025e7
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2022-01-17 09:48:38 +0530

    Second Solution added

--- String-1/makeOutWord.java
+++ String-1/makeOutWord.java
@@ -13,3 +13,17 @@ My Solution :
     public String makeOutWord(String out, String word) {
       return out.substring(0,2) + word + out.substring(2,4);
     }
+
+My Solution 2 : 
+---------------
+    public String makeOutWord(String out, String word) 
+    {
+      String StartStr = "", Endstr = "";
+      int i = 0;
+      if (out.length() == 4)
+      {
+        for (i=0; i<2; i++) StartStr = StartStr + out.charAt(i);
+        for (i=2; i<4; i++) Endstr = Endstr + out.charAt(i);
+      }
+      return StartStr + word + Endstr;
+    }

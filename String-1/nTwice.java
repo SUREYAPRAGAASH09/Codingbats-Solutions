4c4b92e1d5175804f3b702fad654620f27955ed5
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2022-01-19 08:50:57 +0530

    second solution added

--- String-1/nTwice.java
+++ String-1/nTwice.java
@@ -1,6 +1,7 @@
 Problem Statement :
 -------------------
-    Given a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
+    Given a string and an int n, return a string made of the first and last n chars from the string. 
+    The string length will be at least n.
           
 Test Cases :
 ------------
@@ -13,3 +14,20 @@ My Solution :
   public String nTwice(String str, int n) {
     return str.substring(0, n) + str.substring(str.length()-n, str.length()); 
   }
+
+My Solution 2 : 
+---------------
+    public String nTwice(String str, int n) 
+    {
+      String newstr = "";
+      for (int i=0;i<n;i++)
+      {
+        newstr += str.charAt(i);
+      }
+      for (int i=n;i>0;i--)
+      {
+        newstr += str.charAt(str.length() - i);
+      }
+      return newstr;
+    }
+

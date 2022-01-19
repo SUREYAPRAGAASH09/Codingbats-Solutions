124031433d00e2628237468037439c306cdba5f8
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2022-01-19 08:47:46 +0530

    second solution added

--- String-1/middleTwo.java
+++ String-1/middleTwo.java
@@ -1,6 +1,7 @@
 Problem Statement :
 -------------------
-    Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2.
+    Given a string of even length, return a string made of the middle two chars, 
+so the string "string" yields "ri". The string length will be at least 2.
           
 Test Cases :
 ------------
@@ -13,3 +14,22 @@ My Solution :
    public String middleTwo(String str) {
       return (str.length() % 2 == 0) ? str.substring((str.length() / 2) - 1, (str.length() / 2) + 1) : "";
   }
+
+My Solution 2 : 
+---------------
+    public String middleTwo(String str) 
+    {
+      String newstr = "";
+      int strlen = str.length(), UpdatedStrlen = 0, ind = 0;
+      if (strlen % 2 == 0)
+      {
+        UpdatedStrlen = (strlen/2) - 1;
+        while(2>ind)
+        {
+          newstr += str.charAt(UpdatedStrlen);
+          UpdatedStrlen += 1;
+          ind += 1;
+        }
+      }
+      return newstr;
+    }

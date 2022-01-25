f6012b2505c4d28eeb3a75539c3c9d9cd97ad1ed
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2022-01-25 23:52:42 +0530

    Initial commit

--- String-1/Array-1/rotateLeft3.java
+++ String-1/Array-1/rotateLeft3.java
@@ -7,9 +7,9 @@ Problem statement:
 
 Sample testcases:
 -----------------
-  rotateLeft3([1, 2, 3]) → [2, 3, 1]
-  rotateLeft3([5, 11, 9]) → [11, 9, 5]
-  rotateLeft3([7, 0, 0]) → [0, 0, 7]
+  rotateLeft3([1, 2, 3]) → [2, 3, 1].
+  rotateLeft3([5, 11, 9]) → [11, 9, 5].
+  rotateLeft3([7, 0, 0]) → [0, 0, 7].
   
 My solution:
 ------------
